ed32e4ae029d1387ce98fa042d5bd7c9fb60fd49
Евгений <evgeniy.moiseev.progr@mail.ru>
2025-02-13 18:25:27 +0300

    init

---
+++ src/main.py
@@ -0,0 +1,44 @@
+import asyncio
+import logging
+
+from aio_pika import connect_robust
+from aio_pika.abc import AbstractIncomingMessage
+from aio_pika.exceptions import ChannelPreconditionFailed
+from aiofiles import open
+
+from src.settings import settings
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format=settings.LOGGING_FORMAT)
+logger.setLevel(settings.LOGGING_LEVEL)
+
+
+async def process_message(message: AbstractIncomingMessage) -> None:
+    logger.info("Message received")
+    async with message.process():
+        async with open("../logs.txt", "a", encoding="utf-8") as file:
+            await file.write(message.body.decode())
+
+
+async def main() -> None:
+    connection = await connect_robust(settings.get_rmq_url())
+    channel = await connection.channel()
+    await channel.set_qos(prefetch_count=100)
+    queue = await channel.declare_queue(settings.RABBITMQ_QUEUE_NAME)
+
+    await queue.consume(process_message)
+    logger.info("Waiting for messages...")
+
+    try:
+        await asyncio.Future()
+    finally:
+        try:
+            await queue.delete()
+        except ChannelPreconditionFailed:
+            pass
+
+        await connection.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
